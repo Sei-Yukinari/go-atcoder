1e776c1a4d5bb898ebacf4719c09fe4e107c94a4
Sei-Yukinari <sei.yukinari@nextremer.com>
2022-11-10 14:21:12 +0900

    solve math-and-algorithm_009

---
+++ problem/math-and-algorithm/009/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *Io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (io *Io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+func (io *Io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+func (io *Io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) NextInts(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = io.NextInt()
+	}
+	return a
+}
+
+func (io *Io) NextFloat() float64 {
+	i, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) Println(a ...interface{}) {
+	_, err := fmt.Fprintln(io.writer, a...)
+	if err != nil {
+		return
+	}
+}
+
+func (io *Io) Printf(format string, a ...interface{}) {
+	_, err := fmt.Fprintf(io.writer, format, a...)
+	if err != nil {
+		return
+	}
+}
+
+func (io *Io) PrintIntLn(a []int) {
+	var b []interface{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.Println(b...)
+}
+
+func (io *Io) PrintStringLn(a []string) {
+	var b []interface{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.Println(b...)
+}
+
+func Log(name string, value interface{}) {
+	_, err := fmt.Fprintf(os.Stderr, "%s=%+v\n", name, value)
+	if err != nil {
+		return
+	}
+}
+
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func intMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	io := NewIo()
+	defer io.Flush()
+
+	n, s := io.NextInt(), io.NextInt()
+	cards := io.NextInts(n)
+
+	dp := make([][]bool, n+1)
+	for i := range dp {
+		dp[i] = make([]bool, s+1)
+	}
+	dp[0][0] = true
+	for i := 0; i < n; i++ {
+		for j := 0; j <= s; j++ {
+			dp[i+1][j] = dp[i][j]
+			if j >= cards[i] && dp[i][j-cards[i]] {
+				dp[i+1][j] = true
+			}
+		}
+	}
+
+	if dp[n][s] {
+		io.Println("Yes")
+	} else {
+		io.Println("No")
+	}
+
+}
